4b8e9ef635c7954d72186fe67ff2e85babbd96e2
sshaik0976 <Sshaik@alpacasys.com>
2022-04-14 21:07:54 -0600

    CHANGES

--- src/Homepage/Modals.jsx
+++ src/Homepage/Modals.jsx
@@ -1,47 +1,107 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 
 import { Modal, Button } from "antd";
-import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
 
-const Modals = ({ isModalVisible, setIsModalVisible, currentedit,  setcurrentedit }) => {
-  console.log("data",currentedit)
+const Modals = ({
+  isModalVisible,
+  setIsModalVisible,
+  currentedit,
+  setcurrentedit,
+  addpostbyid,
+  state,
+  updatepostbyid,
+}) => {
+  const [currentdata, setcurrentdata] = useState(currentedit); //Holds Current Modal Data
+
   const showModal = () => {
+    setIsModalVisible({ currentstate: true, editing: false });
     setcurrentedit([]);
-    setIsModalVisible(true);
   };
-  useEffect(() => {
-    console.log("current", currentedit)
-
-  }, [currentedit]);
 
   const handleOk = () => {
     setcurrentedit([]);
-
-    setIsModalVisible(false);
+    setIsModalVisible({ currentstate: false, editing: false });
+    if (isModalVisible.editing) {
+      updatepostbyid(state, currentdata);
+    } else {
+      addpostbyid(state, currentdata);
+    }
   };
 
   const handleCancel = () => {
     setcurrentedit([]);
-    setIsModalVisible(false);
+    setIsModalVisible({ currentstate: false, editing: false });
   };
+
+  ///useEffect
+
+  useEffect(() => {
+    if (isModalVisible.editing) {
+      setcurrentdata(currentedit);
+    } else {
+      setcurrentdata({ userId: currentedit.userId, title: "", body: "" });
+    }
+  }, [isModalVisible]);
+
   return (
     <>
-      <Button type="primary" onClick={showModal}>
-      Add Post
-      </Button>
-      <Modal
-        title={<h2>Post:{currentedit.id}</h2>}
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <div style={{display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
-      <div> <span> <b>Title : </b></span><InputText value={currentedit.title}  /> </div> 
-      <div> <span> <b>Description  : </b></span> <InputTextarea  value={currentedit.body}  rows={2} cols={40} autoResize  /> </div> </div>
-      </Modal>{" "}
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ marginLeft: "5rem" }}>
+          <Button style={{ width: "12vw" }} type="primary" onClick={showModal}>
+            Create Post
+          </Button>
+        </div>
+        <Modal
+          title={<h2>Post:{currentdata.id}</h2>}
+          visible={isModalVisible.currentstate}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {" "}
+              <span>
+                {" "}
+                <b>Title : </b>
+              </span>
+              <InputText
+                onChange={(e) => {
+                  setcurrentdata({ ...currentdata, title: e.target.value });
+                }}
+                style={{ width: "20vw", marginLeft: "2rem" }}
+                value={currentdata.title}
+              />{" "}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "2rem",
+              }}
+            >
+              {" "}
+              <span>
+                {" "}
+                <b>Body : </b>
+              </span>{" "}
+              <InputTextarea
+                onChange={(e) => {
+                  setcurrentdata({ ...currentdata, body: e.target.value });
+                }}
+                style={{ width: "20vw", marginLeft: "2rem" }}
+                value={currentdata.body}
+                rows={2}
+                cols={40}
+                autoResize
+              />{" "}
+            </div>{" "}
+          </div>
+        </Modal>{" "}
+      </div>
     </>
   );
 };
